4b8cff083dd0d895bc4ee0adf04175ff01fa6f4f
lindaAcc <lbenboudiaf@accengage.com>
2019-01-09 16:32:10 +0100

    Teste et correction bug

--- src/Blockchain/Hash.java
+++ src/Blockchain/Hash.java
@@ -6,7 +6,7 @@ public class Hash {
 	public static String ApplyHash(String input) {
 		//Lever l'exception.
 		try {
-			MessageDigest digest = new MessageDigest.getInstance("SHA-256"); 
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
 			byte[] hash = digest.digest(input.getBytes("UTF-8")); 
 			StringBuffer hexString = new StringBuffer(); 
 			for(int i = 0; i < hash.length; i++) {
@@ -15,9 +15,10 @@ public class Hash {
 					hexString.append(0); 
 					hexString.append(hex); 
 				}
-				return hexString.toString(); 
 			}
-		}catch(Exception e) {
+			return hexString.toString(); 
+		}
+		catch(Exception e) {
 			throw new RuntimeException(e); 
 		}
 	}
