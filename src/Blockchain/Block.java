c4ac4c8ccd5e559b88f164441aa7fdf577f54f0a
n-peugnet <n.peugnet@free.fr>
2019-01-09 15:17:43 +0100

    first add of blockcahain

---
+++ src/Blockchain/Block.java
@@ -0,0 +1,19 @@
+package Blockchain;
+
+import java.util.Date;
+
+public class Block {
+
+	public String hash;
+	public String previousHash;
+	private String data; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+
+	//Block Constructor.
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+	}
+
+}
