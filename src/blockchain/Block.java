60f3b28c0e25430ddb6d3dc2084406ee4e7593c5
n-peugnet <n.peugnet@free.fr>
2019-01-12 02:00:12 +0100

    méga change of blocks storage mode, not sure if this is a good idea

--- src/blockchain/Block.java
+++ src/blockchain/Block.java
@@ -1,32 +1,56 @@
 
 package blockchain;
 
+import java.io.IOException;
 import java.util.Date;
 import node.Node;
-
+import common.JsonSerialStrategy;
 import common.Log;
+import common.Serializable;
+import common.Storable;
+import common.Store;
 
 /**
  * Class Block {@DATA: date, number}, {@PreviousHash}, {@HashOfCurrentBlock},
  */
-public class Block {
+public class Block implements Storable{
 
-	public String hash;
-	public String previousHash;
-	private String data;
-	private long timeStamp;
-	private int nonce;
-	public Node validator; // on garde une trace du validateur = node qui a validé le block
-	public int reputation; 
+	protected String hash;
+	protected String previousHash;
+	protected String data;
+	protected long timeStamp;
+	protected int nonce;
+	protected Node validator; // on garde une trace du validateur = node qui a validé le block
+	protected int reputation; 
 	
-
-	//Block Constructor.
+	/**
+	 * Constructor for new blocks
+	 * @param data
+	 * @param previousHash
+	 */
 	public Block(String data,String previousHash ) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
+		this.reputation=0;
 		this.hash = calculateHash(); // sera appliquer une fois les autres valuers sont initialsé.
-		this.reputation=0; 
+		mineBlock(Pocket.level);
+	}
+	
+	/**
+	 * Constructor for already minedBlocks
+	 * @param store
+	 * @param hash
+	 * @throws IOException
+	 */
+	public Block(Store store, String hash) throws IOException {
+		this.hash = hash;
+		store.register(this, file(), new JsonSerialStrategy());
+		store.load(file());
+	}
+	
+	public String file() {
+		return "blocks/" + hash;
 	}
 
 	// Calculate new hash based on blocks contents
@@ -44,6 +68,7 @@ public class Block {
 
 	// Increases no value until hash target is reached.
 	public void mineBlock(int difficulty) {
+		Log.debug("Mining Block: " + data);
 		String target = Hash.getDificultyString(difficulty); //Create a string with difficulty * "0" 
 		while(!hash.substring( 0, difficulty).equals(target)) {
 			nonce ++;
@@ -55,6 +80,22 @@ public class Block {
 	@Override
 	public String toString() {
 		return data.toString();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return data.isEmpty() ;
+	}
+
+	@Override
+	public void overwrite(Serializable obj) {
+		Block bloc = (Block) obj;
+		hash = bloc.hash;
+		previousHash = bloc.previousHash;
+		data = bloc.data;
+		timeStamp = bloc.timeStamp;
+		nonce = bloc.nonce;
+		validator = bloc.validator;
 	}
 
 
