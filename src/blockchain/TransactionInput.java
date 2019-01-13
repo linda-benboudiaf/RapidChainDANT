99794b8a03c002e2fb4f0d0d3184336c60e8f9c5
pierrekurz <pkouze@gmail.com>
2019-01-13 18:35:32 +0100

    ajout des classes TransactionInput et TransactionOutput

--- src/blockchain/TransactionInput.java
+++ src/blockchain/TransactionInput.java
@@ -1,5 +1,18 @@
 package blockchain;
+/*
+Transaction inputs are references to previous transaction outputs.
+From this point on we will follow bitcoins convention and call unspent transaction outputs: UTXO’s.
+This class will be used to reference TransactionOutputs that have not yet been spent. 
+The transactionOutputId will be used to find the relevant TransactionOutput, 
+allowing miners to check your ownership.
+ * */
 
 public class TransactionInput {
+	public String transactionOutputId; // Reference to TransactionOutputs -> transactionId
+	public TransactionOutput UTXO; // Contains the Unspent transaction output
+
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
 
 }
--- src/blockchain/TransactionOutput.java
+++ src/blockchain/TransactionOutput.java
@@ -1,5 +1,27 @@
 package blockchain;
 
-public class TransactionOutput {
+import java.security.PublicKey;
 
+/* classe qui represente les transactions envoyees
+ * 
+ */
+public class TransactionOutput {
+	public String id;
+	public PublicKey recipient; //also known as the new owner of these coins.
+	public float value; //the amount of coins they own
+	public String parentTransactionId; //the id of the transaction this output was created in
+	
+	//Constructor
+	public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
+		this.recipient = reciepient;
+		this.value = value;
+		this.parentTransactionId = parentTransactionId;
+		this.id = StringUtil.applySha256(StringUtil.getStringFromKey(reciepient)+Float.toString(value)+parentTransactionId);
+	}
+	
+	//Check if coin belongs to you
+	public boolean isMine(PublicKey publicKey) {
+		return (publicKey == recipient);
+}
+	
 }
